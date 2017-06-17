1059ea94f5b0be6f0bce6430cdf8a0c9b08bb450
Danielss12 <dlb__@hotmail.com>
2017-06-17 13:42:44 +0100

    added circle detection and cascade testing files

---
+++ python_img_processing_tests/test_cascade.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+target_cascade = cv2.CascadeClassifier('cascade18.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, img = cap.read()
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	#faces = face_cascasde.detectMultiScale(gray, 1.3, 5)
+
+	targets = target_cascade.detectMultiScale(gray, 3, 3)
+
+	for (x,y,w,h) in targets:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+		cv2.circle(img,((x+(w/2)),(y+(h/2))),((w/2)),(0,0,255),2)
+		
+
+
+	"""for (x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = img[y:y+h, x:x+w]
+		eyes = eye_cascade.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color,(ex, ey), (ex+ew, ey+eh), (0,255,0), 2)"""
+
+	cv2.imshow('img', img)
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
